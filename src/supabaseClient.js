749460764e7a2c8636473a36fdf28b8046ee8098
Zackdokong <66398105+Zackdokong@users.noreply.github.com>
2025-06-09 15:55:48 +0900

    env만듬

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://jecfqvqrtgtueuqmncms.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImplY2ZxdnFydGd0dWV1cW1uY21zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg4NDU1MzQsImV4cCI6MjA2NDQyMTUzNH0.KusvcMknBxp30Ztnw8ChTaWH6mpU6gTXTXPlJp1etHw';
+const supabaseUrl = `${process.env.SUPABASE_URL}`;
+const supabaseAnonKey = `${process.env.SUPABASE_KEY}`;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
