32a4eaff4b32c0bac65c4d7f3e6ab84867097c32
Zackdokong <66398105+Zackdokong@users.noreply.github.com>
2025-06-18 12:27:14 +0900

    바셀

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Router from "./Router.jsx";
 import { BrowserRouter } from 'react-router-dom';
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <SpeedInsights />
+    <Analytics />
     <BrowserRouter>
       <Router />
     </BrowserRouter>
